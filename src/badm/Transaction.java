801461dad906f779f0d1a07f458389d501f7badb
Colby Rabideau <crabideau5691@gmail.com>
2012-03-22 15:09:10 -0400

    implemented transaction methods

--- src/badm/Transaction.java
+++ src/badm/Transaction.java
@@ -7,6 +7,8 @@ package badm;
 import cc.test.bridge.TransactionInterface;
 import java.util.Date;
 import org.workplicity.entry.Entry;
+import org.workplicity.util.Helper;
+import org.workplicity.worklet.WorkletContext;
 
 /**
  *
@@ -14,34 +16,61 @@ import org.workplicity.entry.Entry;
  */
 public class Transaction extends BaseModel implements TransactionInterface {
 
+	protected Integer sublineId;
+	protected String name;
+	protected Double amount;
+	protected Date date;
+	
+	public Integer getSublineId() {
+		return sublineId;
+	}
+	
+	public void setSublineId(Integer sid) {
+		this.sublineId = sid;
+	}
+	
+	public Subline getSubline() {
+		return Subline.find(sublineId);
+	}
+	
+	public void setSubline(Subline subline) {
+		this.sublineId = subline.getId();
+	}
+	
 	@Override
 	public Date getDate() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return date;
 	}
 
 	@Override
 	public void setDate(Date date) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		this.date = date;
 	}
 
 	@Override
 	public Double getAmount() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return amount;
 	}
 
 	@Override
-	public void setAmount(Double d) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public void setAmount(Double amount) {
+		this.amount = amount;
 	}
 
 	@Override
-	public void setName(String string) {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
 	public String getName() {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return name;
 	}
 
+	public static Transaction find(Integer id) {
+		Transaction result;
+		result = (Transaction) Helper.fetch("Transactions", id, context());
+		return result;
+	}
+	
 }
