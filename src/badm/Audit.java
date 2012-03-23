f08fb9ca8c5000e734ebc702eaa452003b462fe9
Thomas Crescenzi <trescenzi@gmail.com>
2012-03-22 23:20:06 -0400

    added Audit

---
+++ src/badm/Audit.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package badm;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author idontknow5691
+ */
+public class Audit extends BaseModel {
+
+	public int timestamp;
+	
+	public ArrayList<Integer> updated;
+	
+	public String description;
+        
+        protected Integer budgetId;
+
+        public Integer getBudgetId() {
+            return budgetId;
+        }
+
+        public void setBudgetId(Integer budgetId) {
+            this.budgetId = budgetId;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public ArrayList<Integer> getUpdated() {
+            return updated;
+        }
+
+        public void setUpdated(ArrayList<Integer> updated) {
+            this.updated = updated;
+        }
+	
+	public int getTimestamp()
+	{
+		return timestamp;
+	}
+	
+}
