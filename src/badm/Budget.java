74b468ae57ec3c88df12917b2d263880eb528d94
Thomas Crescenzi <trescenzi@gmail.com>
2012-03-23 10:12:48 -0400

    Commented Budget and fixed a few bugs found in the process

--- src/badm/Budget.java
+++ src/badm/Budget.java
@@ -16,33 +16,55 @@ import org.workplicity.worklet.WorkletContext;
 
 /**
  * Budget
- * 
+ * The base of the database. Everything is in some way a part of a Budget.
  * @author Trescenzi
  */
 public class Budget extends BaseModel implements BudgetInterface {
     protected String description;
     protected String name;
     protected Integer total;
-
+    
+    /**
+     * 
+     * @return The total value of the budget
+     */
     public Integer getTotal() {
         return total;
     }
 
+    /**
+     * 
+     * @param total The new total value of the budget
+     */
     public void setTotal(Integer total) {
         this.total = total;
     }
     
     
+    /**
+     * 
+     * @return The budget's description
+     */
     @Override
     public String getDescription() {
         return description;
     }
-
+    
+    /**
+     * 
+     * @param d The new description for the budget
+     */
     @Override
     public void setDescription(String d) {
         description = d;
     }
-
+    
+    /**
+     * Gets all of the lines associated with the budget.
+     * Does not take into account side yet.
+     * @param side Expenditures/Income
+     * @return The lines associated with the budget
+     */
     @Override
     public ArrayList<LineInterface> fetchLines(Side side) {
         BasicDBObject query = new BasicDBObject();
@@ -54,7 +76,11 @@ public class Budget extends BaseModel implements BudgetInterface {
         }
         return null;
     }
-
+    
+    /**
+     * Gets all of the notes associated with the budget.
+     * @return The Notes associated with the budget
+     */
     @Override
     public ArrayList<NoteInterface> fetchNotes() {
         BasicDBObject query = new BasicDBObject();
@@ -66,86 +92,141 @@ public class Budget extends BaseModel implements BudgetInterface {
         }
         return null;
     }
-
+    
+    /**
+     * Makes a new Line, associates the line with the calling budget
+     * then returns the Line.
+     * @return The new Line
+     */
     @Override
     public LineInterface createLine() {
 	Line ln = new Line();
 	ln.setBudgetId(id);
         return ln;
     }
-
+    
+    /**
+     * Makes a new Note, associates the Note with the calling budget
+     * then returns the Note.
+     * @return The new Note
+     */
     @Override
     public NoteInterface createNote() {
 	Note n = new Note();
 	n.setBudgetId(id);
         return n;
     }
-
+    
+    /**
+     * Associates a Note with the calling Budget.
+     * @param ni The Note to associate
+     */
     @Override
     public void add(NoteInterface ni) {
         Note note = (Note) ni;
         note.setBudgetId(this.id);
     }
-
+    
+    /**
+     * Deletes a Note.
+     * @param ni The Note to delete
+     */
     @Override
     public void delete(NoteInterface ni) {
         Note note = (Note) ni;
-        Helper.delete(note, note.getRepositoryName(), WorkletContext.getInstance());
+        note.delete();
     }
-
+    
+    /**
+     * Associates a Line with the calling Budget.
+     * @param li The Line to associate
+     */
     @Override
     public void add(LineInterface li) {
         Line line = (Line) li;
         line.setBudgetId(this.id);
     }
-
+    
+    /**
+     * Deletes a Line.
+     * @param li The Line to delete
+     */
     @Override
     public void delete(LineInterface li) {
         Line line = (Line) li;
-        Helper.delete(line, line.getRepositoryName(), WorkletContext.getInstance());
+        line.delete();
     }
-
+    
+    /**
+     * Updates a Line. Commits upon call.
+     * @param li The Line to be updated
+     */
     @Override
     public void update(LineInterface li) {
         Line line = (Line) li;
-        Helper.insert(line, line.getRepositoryName(), WorkletContext.getInstance());
+        line.commit();
     }
-
+    
+    /**
+     * Updates a Note. Commits upon call.
+     * @param ni The Note to be updated
+     */
     @Override
     public void update(NoteInterface ni) {
-        
+        Note note = (Note) ni;
+        note.commit();
     }
 
+    /**
+     * 
+     * @param string The new name of the Budget
+     */
     @Override
     public void setName(String string) {
         name = string;
     }
 
+    /**
+     * 
+     * @return The name of the Budget 
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * 
+     * @return A WorkDate?
+     */
     @Override
     public org.workplicity.util.WorkDate getUpdateDate() {
         return null;
     }
 
-    public BudgetInterface create() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
+    /**
+     * 
+     * @param audit The audit to be added
+     */
     private void addAudit(Audit audit){
         audit.setBudgetId(id);
     }
     
+    /**
+     * Updates the Budget then stashes the Audit in AuditLand.
+     * @param audit The Audit to use as a refrence for the update
+     */
     public void update(Audit audit){
         super.update(audit);
         //TODO add totals and stuffs
         addAudit(audit);
     }
     
-
+    /**
+     * Finds a budget with a specific id.
+     * @param id The id of the Budget to be found
+     * @return The Budget, of null if not found.
+     */
     public static Budget find(Integer id) {
 	    BasicDBObject query = new BasicDBObject();
 	    query.put("entry.id", id);
