486a9fa49d05f2fbd52b68875bf570ed1940fbb7
Colby Rabideau <crabideau5691@gmail.com>
2012-03-23 10:48:34 -0400

    audit comments

--- src/badm/Audit.java
+++ src/badm/Audit.java
@@ -20,30 +20,76 @@ public class Audit extends BaseModel {
 	public String description;
 	protected Integer budgetId;
 
+	/**
+	 * get budget id
+	 * 
+	 * returns id of parent budget
+	 * 
+	 * @return 
+	 */
 	public Integer getBudgetId() {
 		return budgetId;
 	}
 
+	/**
+	 * set budget id
+	 * 
+	 * sets parent budget id
+	 * 
+	 * @param budgetId 
+	 */
 	public void setBudgetId(Integer budgetId) {
 		this.budgetId = budgetId;
 	}
 
+	/**
+	 * get description
+	 * 
+	 * gets audit description
+	 * 
+	 * @return 
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * set description
+	 * 
+	 * sets audit description
+	 * 
+	 * @param description 
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * get updated
+	 * 
+	 * returns array list of update chain
+	 * 
+	 * @return 
+	 */
 	public ArrayList<Integer> getUpdated() {
 		return updated;
 	}
 
+	/**
+	 * set updated
+	 * 
+	 * set updated chain array list
+	 * 
+	 * @param updated 
+	 */
 	public void setUpdated(ArrayList<Integer> updated) {
 		this.updated = updated;
 	}
 
+	/**
+	 * get audit timestamp
+	 * @return 
+	 */
 	public int getTimestamp() {
 		return timestamp;
 	}
