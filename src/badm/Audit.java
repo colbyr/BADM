0cf6f4d42b3da0ceff344df1f5f713c9deb9bb45
Colby Rabideau <crabideau5691@gmail.com>
2012-03-23 00:00:16 -0400

    audit got all weird...

--- src/badm/Audit.java
+++ src/badm/Audit.java
@@ -7,63 +7,45 @@ package badm;
 import java.util.ArrayList;
 
 /**
+ * Audit
  *
- * @author idontknow5691
+ * Model for tracking budget changes
+ *
+ * @author Colby Rabideau
  */
 public class Audit extends BaseModel {
 
 	public int timestamp;
-	
-	public ArrayList<Integer> updated;
-	
-	public String description;
-        
-        protected Integer budgetId;
-
-        public Integer getBudgetId() {
-            return budgetId;
-        }
-
-        public void setBudgetId(Integer budgetId) {
-            this.budgetId = budgetId;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public ArrayList<Integer> getUpdated() {
-            return updated;
-        }
-
-        public void setUpdated(ArrayList<Integer> updated) {
-            this.updated = updated;
-        }
-	public Integer budgetId;
 	public ArrayList<Integer> updated;
 	public String description;
+	protected Integer budgetId;
+
+	public Integer getBudgetId() {
+		return budgetId;
+	}
 
 	public void setBudgetId(Integer budgetId) {
 		this.budgetId = budgetId;
 	}
-	
-	public Integer getBudgetId() {
-		return budgetId;
-	}
-	
-	public int getTimestamp()
-	{
-		return timestamp;
+
+	public String getDescription() {
+		return description;
 	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-	public ArrayList getUpdated()
-	{
+	public ArrayList<Integer> getUpdated() {
 		return updated;
 	}
 
+	public void setUpdated(ArrayList<Integer> updated) {
+		this.updated = updated;
+	}
+
+	public int getTimestamp() {
+		return timestamp;
+	}
+	
 }
