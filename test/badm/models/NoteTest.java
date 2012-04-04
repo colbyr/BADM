b8fb4c5f99c309fde7293169cbf4e4ee19ac314f
Colby Rabideau <crabideau5691@gmail.com>
2012-04-04 18:39:19 -0400

    note tests

--- test/badm/models/NoteTest.java
+++ test/badm/models/NoteTest.java
@@ -26,108 +26,62 @@ public class NoteTest {
 		}
 	}
 
+	public Note note;
+	
 	@AfterClass
 	public static void tearDownClass() throws Exception {
 	}
 	
 	@Before
 	public void setUp() {
+		note = new Note();
+		note.setAuthor("Colby");
+		note.setName("Test");
+		note.setBudgetId(1);
+		note.commit();
 	}
 	
 	@After
 	public void tearDown() {
+		note.delete();
 	}
 
-	/**
-	 * Test of setBudgetId method, of class Note.
-	 */
 	@Test
-	public void testBudgetId() {
-		System.out.println("setBudgetId");
-		Integer id = 10;
-		Note instance = new Note();
-		instance.setBudgetId(id);
-		assertEquals(id, instance.getBudgetId());
+	public void testCreateNote()
+	{
+		assertTrue(note.getId() > -1);
 	}
-
-//	/**
-//	 * Test of setText method, of class Note.
-//	 */
-//	@Test
-//	public void testSetText() {
-//		System.out.println("setText");
-//		String t = "";
-//		Note instance = new Note();
-//		instance.setText(t);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
-//
-//	/**
-//	 * Test of getAuthor method, of class Note.
-//	 */
-//	@Test
-//	public void testGetAuthor() {
-//		System.out.println("getAuthor");
-//		Note instance = new Note();
-//		String expResult = "";
-//		String result = instance.getAuthor();
-//		assertEquals(expResult, result);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
-//
-//	/**
-//	 * Test of setAuthor method, of class Note.
-//	 */
-//	@Test
-//	public void testSetAuthor() {
-//		System.out.println("setAuthor");
-//		String string = "";
-//		Note instance = new Note();
-//		instance.setAuthor(string);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
-//
-//	/**
-//	 * Test of setName method, of class Note.
-//	 */
-//	@Test
-//	public void testSetName() {
-//		System.out.println("setName");
-//		String string = "";
-//		Note instance = new Note();
-//		instance.setName(string);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
-//
-//	/**
-//	 * Test of getName method, of class Note.
-//	 */
-//	@Test
-//	public void testGetName() {
-//		System.out.println("getName");
-//		Note instance = new Note();
-//		String expResult = "";
-//		String result = instance.getName();
-//		assertEquals(expResult, result);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
-//
-//	/**
-//	 * Test of find method, of class Note.
-//	 */
-//	@Test
-//	public void testFind() {
-//		System.out.println("find");
-//		Integer id = null;
-//		Note expResult = null;
-//		Note result = Note.find(id);
-//		assertEquals(expResult, result);
-//		// TODO review the generated test code and remove the default call to fail.
-//		fail("The test case is a prototype.");
-//	}
+	
+	@Test
+	public void testSetAndSaveAttributes()
+	{
+		assertEquals("Colby", note.getAuthor());
+		assertEquals("Test", note.getName());
+		assertEquals(new Integer(1), note.getBudgetId());
+	}
+	
+	@Test
+	public void testUpdateAttribtues()
+	{
+		note.setAuthor("Tom");
+		assertTrue(note.commit());
+		
+		System.out.println("fetching note with id: " + note.getId());
+		
+		Note new_note = Note.find(note.getId());
+		assertNotNull(new_note);
+		assertEquals(note.getAuthor(), new_note.getAuthor());
+	}
+	
+	@Test
+	public void testDeleteNote()
+	{
+		assertTrue(note.delete());
+	}
+	
+	@Test
+	public void testFalse()
+	{
+		assertFalse(false);
+	}
 }
