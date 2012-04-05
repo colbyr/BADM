79b616551eebd6db895fc171b758813c737e02fb
Colby Rabideau <crabideau5691@gmail.com>
2012-04-05 03:23:10 -0400

    Comments and the like

--- src/badm/Transaction.java
+++ src/badm/Transaction.java
@@ -12,100 +12,102 @@ import org.workplicity.util.MongoHelper;
 
 /**
  * A Transaction. This represents a single change in value for a Budget.
- * Depending on the type of the Budget and how often the Budget is to be
- * updated the number of Transactions will change. The only place that a number
- * amount can change is in a Transaction and the change will then "bubble up" as
- * an Audit changing the values of other objects as it goes.
- * @author idontknow5691
+ * Depending on the type of the Budget and how often the Budget is to be updated
+ * the number of Transactions will change. The only place that a number amount
+ * can change is in a Transaction and the change will then "bubble up" as an
+ * Audit changing the values of other objects as it goes.
+ *
+ * @author Colby Rabideau
  */
 public class Transaction extends BaseModel implements TransactionInterface {
 
 	protected Integer sublineId;
 	protected Double amount;
 	protected Date date;
-	
-        /**
-         * 
-         * @return The id of the linked Subline
-         */
+
+	/**
+	 *
+	 * @return The id of the linked Subline
+	 */
 	public Integer getSublineId() {
 		return sublineId;
 	}
-	
-        /**
-         *  Associates a Transaction with a Subline. Must be called if the 
-         * Transaction is to be paired with a Subline unless you already
-         * called Subline.createTransaction() or Subline.add(Transaction) with this
-         * specific Transaction.
-         * @param sid The Subline to link to
-         */
+
+	/**
+	 * Associates a Transaction with a Subline. Must be called if the
+	 * Transaction is to be paired with a Subline unless you already called
+	 * Subline.createTransaction() or Subline.add(Transaction) with this
+	 * specific Transaction.
+	 *
+	 * @param sid The Subline to link to
+	 */
 	public void setSublineId(Integer sid) {
 		this.sublineId = sid;
-                dirty();
+		dirty();
 	}
-	
+
 	/**
-         * 
-         * @return Date of creation 
-         */
+	 *
+	 * @return Date of creation
+	 */
 	@Override
 	public Date getDate() {
 		return date;
 	}
 
-        /**
-         * 
-         * @param date The new Date 
-         */
+	/**
+	 *
+	 * @param date The new Date
+	 */
 	@Override
 	public void setDate(Date date) {
 		this.date = date;
-                dirty();
+		dirty();
 	}
 
-        /**
-         * 
-         * @return The value of the Transaction
-         */
+	/**
+	 *
+	 * @return The value of the Transaction
+	 */
 	@Override
 	public Double getAmount() {
 		return amount;
 	}
 
-        /**
-         * 
-         * @param amount New amount
-         */
+	/**
+	 *
+	 * @param amount New amount
+	 */
 	@Override
 	public void setAmount(Double amount) {
 		this.amount = amount;
-                Audit audit = new Audit();
-                audit.setValue(amount);
-                update(audit);
+		Audit audit = new Audit();
+		audit.setValue(amount);
+		update(audit);
 	}
-        
-        @Override
-        public void update(Audit audit){
-            super.update(audit);
-            audit.setDescription("Change in transaction "+ this.getId() + " " + audit.getValue());
-            dirty();
-            Subline.find(sublineId).update(audit);
-        }
-        
-        /**
-         * Finds a Transaction.
-         * @param id The id of the Transaction to be found.
-         * @return The Transaction or null if not found
-         */
+
+	@Override
+	public void update(Audit audit) {
+		super.update(audit);
+		audit.setDescription("Change in transaction " + this.getId() + " " + audit.getValue());
+		dirty();
+		Subline.find(sublineId).update(audit);
+	}
+
+	/**
+	 * Finds a Transaction.
+	 *
+	 * @param id The id of the Transaction to be found.
+	 * @return The Transaction or null if not found
+	 */
 	public static Transaction find(Integer id) {
-            BasicDBObject query = new BasicDBObject();
-            query.put("entry.id", id);
-            try{
-                return (Transaction) MongoHelper.query(query,BaseModel.getStoreName(),new Transaction().getRepositoryName()).get(0);
-            }catch(Exception e){
-                    System.out.println("couldnt find Subline #"+id+" "+e);
-            }
-            return null;	
-        }
-	
+		BasicDBObject query = new BasicDBObject();
+		query.put("entry.id", id);
+		try {
+			return (Transaction) MongoHelper.query(query, BaseModel.getStoreName(), new Transaction().getRepositoryName()).get(0);
+		} catch (Exception e) {
+			System.out.println("couldnt find Subline #" + id + " " + e);
+		}
+		return null;
+	}
 }
